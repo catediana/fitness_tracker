b2266848af81e25b436cb7abe1aaaa57fc7997df
catediana <catherinenanyala@gmail.com>
2025-01-02 10:22:54 -0800

    created future_age_calculator.py file and calculated future age in 2025

---
+++ python/python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+#prompt to input currect age
+current_age = int(input("How old are you? "))
+
+#calculating the future year
+future_age = current_age + (2050-2030)
+
+print(f"In 2025, you will be {future_age} years old.")
